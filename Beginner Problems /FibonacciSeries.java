842bc1f9da2b4464992d1383f2755190c5a03e65
Pooja Mankar <144476885+poojamankar30@users.noreply.github.com>
2025-11-16 21:49:31 +0530

    Create FibonacciSeries.java

---
+++ Beginner Problems /FibonacciSeries.java
@@ -0,0 +1,16 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 10;   // number of terms
+
+        int a = 0, b = 1;
+
+        System.out.print(a + " " + b + " ");
+
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}
