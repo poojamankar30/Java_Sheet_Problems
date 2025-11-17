60ef4d76fd357ab5ddeb01840691c1d9e4b4b5da
Pooja Mankar <144476885+poojamankar30@users.noreply.github.com>
2025-11-18 00:52:38 +0530

    Create Palindrome.java

---
+++ Beginner Problems /Palindrome.java
@@ -0,0 +1,13 @@
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String str = "madam";
+
+        String reversed = new StringBuilder(str).reverse().toString();
+
+        if(str.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
