806095f8281e2e3b9f971e20aba4bf0569f69c7b
Pooja Mankar <144476885+poojamankar30@users.noreply.github.com>
2025-11-19 22:44:50 +0530

    Create MinMaxInarr.java

---
+++ Beginner Problems /MinMaxInarr.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MinMaxInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int smallest = arr[0];
+        int largest = arr[0];
+
+        // Find smallest and largest
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Smallest number = " + smallest);
+        System.out.println("Largest number  = " + largest);
+    }
+}
